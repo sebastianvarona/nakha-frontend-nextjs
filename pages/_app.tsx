3f7a9758c2c3831e78fbc99428c8ec3e2d0c9889
sebastianvarona <sebastianvarona@gmail.com>
2023-09-03 23:16:22 -0500

    added comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,6 +43,7 @@ const PayloadApp = (
   return (
     <AuthProvider>
       <Head>
+        {/* Favicon Files */}
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
