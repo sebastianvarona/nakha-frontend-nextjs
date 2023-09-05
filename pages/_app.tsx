276c4d3b9c60543d836526847392633d64f69c7c
sebastianvarona <sebastianvarona@gmail.com>
2023-09-05 14:49:05 -0500

    added fonts via html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,6 +48,12 @@ const PayloadApp = (
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@400;700&family=Quattrocento:wght@400;700&family=Raleway:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <CartProvider>
         <GridProvider
