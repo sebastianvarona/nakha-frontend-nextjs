0c6fd01fb956a2dd58bd6bec6055b75a5c88fcd2
sebastianvarona <sebastianvarona@gmail.com>
2023-09-06 14:14:01 -0500

    added open graph support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,12 +49,50 @@ const PayloadApp = (
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <meta name="theme-color" content="#000000" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+        {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Quattrocento+Sans:wght@400;700&family=Quattrocento:wght@400;700&family=Raleway:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        {/* Open Graph */}
+
+        {/* <!-- HTML Meta Tags --> */}
+        <title>
+          NAKHA | Taste the Difference - Discover the essence of premium Colombian coffee
+        </title>
+        <meta
+          name="description"
+          content="At NAKHA, our philosophy is simple: to bring people together over exceptional coffee. We believe that coffee is more than just a drink - it's a catalyst for connection, conversation, and community. That's why we are dedicated to crafting the finest blends from around the world, using only the highest quality beans and roasting methods. But our commitment to excellence goes beyond just the coffee itself."
+        />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://www.nakha.coffee" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="NAKHA | Taste the Difference - Discover the essence of premium Colombian coffee"
+        />
+        <meta
+          property="og:description"
+          content="At NAKHA, our philosophy is simple: to bring people together over exceptional coffee. We believe that coffee is more than just a drink - it's a catalyst for connection, conversation, and community. That's why we are dedicated to crafting the finest blends from around the world, using only the highest quality beans and roasting methods. But our commitment to excellence goes beyond just the coffee itself."
+        />
+        <meta property="og:image" content="https://www.nakha.coffee/open-graph-min.jpeg" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="nakha.coffee" />
+        <meta property="twitter:url" content="https://www.nakha.coffee" />
+        <meta
+          name="twitter:title"
+          content="NAKHA | Taste the Difference - Discover the essence of premium Colombian coffee"
+        />
+        <meta
+          name="twitter:description"
+          content="At NAKHA, our philosophy is simple: to bring people together over exceptional coffee. We believe that coffee is more than just a drink - it's a catalyst for connection, conversation, and community. That's why we are dedicated to crafting the finest blends from around the world, using only the highest quality beans and roasting methods. But our commitment to excellence goes beyond just the coffee itself."
+        />
+        <meta name="twitter:image" content="https://www.nakha.coffee/open-graph-min.jpeg" />
       </Head>
       <CartProvider>
         <GridProvider
