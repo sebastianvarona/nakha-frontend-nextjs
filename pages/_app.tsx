5dfa589499c24a1b6b5faf6ef59d398b8ec74100
sebastianvarona <sebastianvarona@gmail.com>
2023-09-06 10:15:42 -0500

    corrected banner height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,6 +47,7 @@ const PayloadApp = (
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <meta name="theme-color" content="#000000" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
